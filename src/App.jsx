0d635c926134708fd408178630f979194192a0d3
adithyharivpn <adithyharivpn@gmail.com>
2025-04-25 15:28:00 +0530

    updated

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,12 @@
 
 import { Route, Routes } from 'react-router-dom'
 import './App.css'
-import NavBar from './components/NavBar'
 import SignUp from './components/SignUp'
 import Login from './components/Login'
 import Main from './components/Main'
 import Admin from './components/Admin'
 import Product from './components/Product'
+import ProductDetails from './components/ProductDetails'
 
 function App() {
   
@@ -20,6 +20,7 @@ function App() {
         <Route path='/signup' element={<SignUp/>} />
         <Route path='/admin' element={<Main child={<Admin/>}/>}/>
         <Route path='/product' element={<Main child={<Product/>}/>}/>
+        <Route path='/details' element={<Main child={<ProductDetails/>}/>}/>
       </Routes>
     </>
   )
--- src/components/Admin.jsx
+++ src/components/Admin.jsx
@@ -11,54 +11,60 @@ const Admin = () => {
     stock: "",
     images: [],
   });
-      var baseurl = import.meta.env.VITE_API_BASE_URL;
-      const inputHandler = (e)=>{
-          setInput({...input,[e.target.name]:e.target.value})
-          console.log(input)
-      } 
+  var baseurl = import.meta.env.VITE_API_BASE_URL;
+  const inputHandler = (e) => {
+    setInput({ ...input, [e.target.name]: e.target.value });
+    console.log(input);
+  };
 
-      const setHandler = ()=>{
-        const formdata = new FormData();
-        formdata.append("pname",input.pname)
-        formdata.append("price",input.price)
-        formdata.append("description",input.description)
-        formdata.append("stock",input.stock)
-        input.images.forEach((file)=>{
-          formdata.append('images',file)
-        })
-        axios
-        .post(`${baseurl}/p`,formdata)
-        .then((res) => {
-          console.log(res.data);
-          alert(res.data.message)
-        
-        })
-        .catch((error) => {
-          console.log(error);
-        })
-      }
+  const setHandler = () => {
+    const formdata = new FormData();
+    formdata.append("pname", input.pname);
+    formdata.append("price", input.price);
+    formdata.append("description", input.description);
+    formdata.append("stock", input.stock);
+    input.images.forEach((file) => {
+      formdata.append("images", file);
+    });
+    axios
+      .post(`${baseurl}/p`, formdata)
+      .then((res) => {
+        console.log(res.data);
+        alert(res.data.message);
+      })
+      .catch((error) => {
+        console.log(error);
+      });
+  };
   return (
-    <div style={{ paddingLeft: 420 }}>
-            <Button variant="contained">
-        <Link to="/d" style={{ textDecoration: "none", color: "white" }}>
-          Product Details
-        </Link>
-      </Button>
+    <div style={{margin:7}}>
+    <Button variant="contained">
+    <Link to="/details" style={{ textDecoration: "none", color: "white"}}>
+      Product Details
+    </Link>
+  </Button>
 
+
+
+    <Box
+      sx={{
+        display: "flex",
+        justifyContent: "center",
+        alignItems: "center",
+        minHeight: "100vh",
+        bgcolor: "#f5f5f5",
+      }}
+    >
       <Box
         component="form"
         sx={{
-          padding: 2,
+          p: 3,
           backgroundColor: "white",
           boxShadow: 3,
-          marginTop: 4,
-          width: 400,
-          display: "flex",
-          alignContent: "center",
-          borderRadius:2
+          width: { xs: "70%", sm: 400 },
+          borderRadius: 2,
         }}
       >
-        
         <div>
           <Typography variant="h3" sx={{ color: "black" }}>
             Product Form
@@ -108,39 +114,45 @@ const Admin = () => {
             name="category"
             onChange={inputHandler}
           >
-            <MenuItem value={'10'}>test</MenuItem>
-            <MenuItem value={'20'}>test</MenuItem>
-            <MenuItem value={'30'}>test</MenuItem>
+            <MenuItem value={"10"}>test</MenuItem>
+            <MenuItem value={"20"}>test</MenuItem>
+            <MenuItem value={"30"}>test</MenuItem>
           </TextField>
-          <FormControlLabel control={<Switch defaultChecked color="warning"/>} label="Available" sx={{color:'black'}}/>
-          <Button variant='outlined' component='label'>
+          <FormControlLabel
+            control={<Switch defaultChecked color="warning" />}
+            label="Available"
+            sx={{ color: "black" }}
+          />
+          <Button variant="outlined" component="label">
             Upload images
             <input
-            type='file'
-            hidden
-            multiple
-            accept='image/*'
-            // onChange={(e)=>{
-            //   setInput({...input,images:Array.from(e.target.files)})
-            // }}//loses values 
-            onChange={(e) => {
-              const files = Array.from(e.target.files);
-              setInput((prev) => ({
-                ...prev,
-                images: files,
-              }))}}//stores prvious states
+              type="file"
+              hidden
+              multiple
+              accept="image/*"
+              // onChange={(e)=>{
+              //   setInput({...input,images:Array.from(e.target.files)})
+              // }}//loses values
+              onChange={(e) => {
+                const files = Array.from(e.target.files);
+                setInput((prev) => ({
+                  ...prev,
+                  images: files,
+                }));
+              }} //stores prvious states
             />
           </Button>
-          <Typography variant="caption" display="block" color='black'>
-                  {input.images.length} file(s) selected
-                </Typography>
+          <Typography variant="caption" display="block" color="black">
+            {input.images.length} file(s) selected
+          </Typography>
           <Button fullWidth variant="contained" onClick={setHandler}>
             submit
           </Button>
         </div>
       </Box>
+    </Box>
     </div>
   );
-}
+};
 
-export default Admin
+export default Admin;
